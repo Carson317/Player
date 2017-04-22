14a04f75932c8a6df37ea93b596f7a718e4ad37e
heng cao <heng cao>
2017-04-22 18:05:27 +0800

    add splash activity

---
+++ app/src/main/java/com/carson/player/MainActivity.java
@@ -0,0 +1,18 @@
+package com.carson.player;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by heng on 2017/4/22.
+ */
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/carson/player/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.carson.player;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startMainActivity();
+            }
+        }, 3000);
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
