fb6b47257e8390f56d674fc6e2f9e181fe7b7e0d
heng cao <heng cao>
2017-04-23 15:54:08 +0800

    hideStatusBar

--- app/src/main/java/com/carson/player/MainActivity.java
+++ app/src/main/java/com/carson/player/MainActivity.java
@@ -2,6 +2,7 @@ package com.carson.player;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by heng on 2017/4/22.
@@ -13,6 +14,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = new TextView(this,null);
+        textView.setText("主页面");
+        textView.setTextSize(18);
 
     }
 }
--- app/src/main/java/com/carson/player/SplashActivity.java
+++ app/src/main/java/com/carson/player/SplashActivity.java
@@ -4,23 +4,32 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends Activity {
+
+    private static final String TAG = "SplashActivity";
 
     private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        hideStatusBar();
         setContentView(R.layout.activity_splash);
 
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-
                 startMainActivity();
             }
         }, 3000);
+    }
+
+    private void hideStatusBar() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
     private void startMainActivity() {
@@ -33,6 +42,7 @@ public class SplashActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+        //可以才启动过程做一些其他事情
     }
 
     @Override
